5f18dfcbc52aa965d16b0606a735c5a63eb44bba
MattW-9393 <matt.warne22@gmail.com>
2025-11-11 08:34:34 +0000

    updated if statement error. moderate rating now skips commentary

--- tbc_tracker.py
+++ tbc_tracker.py
@@ -15,7 +15,7 @@ name = "Matt"
 def daily_mood_check():
     print("""Hi, welcome to your mood checker.
           I'd like to ask you a couple of questions about your mood if that's okay?""")
-    mood_rating = int(input("On a scale of 1-10, how do you feel today? 1 being 'Very Unhappy' and 10 being 'Very Happy'"))
+    mood_rating = int(input("On a scale of 1-10, how do you feel today? 1 being 'Very Unhappy' and 10 being 'Very Happy' /n"))
 
     match mood_rating:
         case 1 | 2:
@@ -29,21 +29,21 @@ def daily_mood_check():
         case 9 | 10:
             mood_tag[0]
     
-    if mood_tag == mood_tag[4] or mood_tag[3] or mood_tag[1] or mood_tag[0]:
-        commentary = input("Please add some commentary explaining your answer (250 char max)")
+    if mood_tag == mood_tag[4] or mood_tag == mood_tag[3] or mood_tag == mood_tag[1] or mood_tag == mood_tag[0]:
+        commentary = input("Please add some commentary explaining your answer (250 char max): /n")
     else:
         print(no_comment_required)
     
-    print("Thanks! I've saved your records. What would you like to do next?") 
-    what_next = int(input("Press '1' to view all your entries so far, or Press '0' to exit the program"))
+    print("Thanks! I've saved your records. What would you like to do next? /n") 
+    what_next = int(input("Press '1' to view all your entries so far, or Press '0' to exit the program /n"))
 
     if what_next == 1:
         #display database records
-        print("Here are your previous entries")
+        print("Here are your previous entries: /n")
     elif what_next == 0:
         #exit function
         print("Thanks, see you again tomorrow!")
-    else: "Invalid entry, please press '1' to view your previous records, or press '0' to exit the program"
+    else: "Invalid entry, please press '1' to view your previous records, or press '0' to exit the program /n"
 
     # Need to add various DB features to send this information somewhere...
 
--- toDo.md
+++ toDo.md
@@ -1,3 +1,3 @@
 To Do list
 
-1. Fix the prompt for commentary when user input's "Moderate" happiness. There is a default answer for this mood tag that should be used.
+1. ~~Fix the prompt for commentary when user input's "Moderate" happiness. There is a default answer for this mood tag that should be used.~~
