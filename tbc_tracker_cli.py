b997cae92ce025656cce4ab4d587258e73cc5bf2
MattW-9393 <matt.warne22@gmail.com>
2025-11-14 16:11:15 +0000

    refactor code so input handling isnt so fragile

--- tbc_tracker_cli.py
+++ tbc_tracker_cli.py
@@ -1,100 +1,113 @@
 import datetime
 import sqlite3
 import csv
-from flask import Flask
-
-app = Flask(__name__)
-
-# Flask Server
-@app.route("/")
-def display_data():
-    return "<p>Hello, World!</p>"
-
-# Set up Mood-Journal DB
-con = sqlite3.connect("proto-mood.db")
-cur = con.cursor()
-res = cur.execute("SELECT name FROM sqlite_master")
-
-if con == None: 
-   cur.execute("CREATE TABLE MoodRatings (date, moodLabel, commentary)")
-
 
 # Get today's date and save as variable
-xdate = datetime.datetime.now()
-the_date = xdate.strftime("%d-%m-%Y")
+
 
 mood_tag = ["Very Happy", "Happy", "Moderate", "Unhappy", "Very Unhappy"]
 negative_secondary_mood = ["Anxious", "Stressed", "Angry", "Sad"]
 positive_secondary_mood = ["Excited", "Satisfied", "Relieved"]
-todays_mood = ""
 
 name = "Bob"
 
+
 def daily_mood_check():
-    print(f"""Hi {name}, welcome to your mood checker.
-          I'd like to ask you a couple of questions about your mood if that's okay?""")
-    mood_rating = int(input("On a scale of 1-10, how do you feel today? 1 being 'Very Unhappy' and 10 being 'Very Happy: "))
+    xdate = datetime.datetime.now()
+    the_date = xdate.strftime("%d-%m-%Y")
+    with sqlite3.connect("proto-mood.db") as con:
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS MoodRatings (date, mood_label, commentary)")
+        todays_mood = ""
+        print(f"""Hi {name}, welcome to your mood checker.
+              I'd like to ask you a couple of questions about your mood if that's okay?""")
 
-    match mood_rating:
-        case 1 | 2:
-            todays_mood = mood_tag[4]
-        case 3 | 4:
-            todays_mood = mood_tag[3]
-        case 5 | 6:
-            todays_mood = mood_tag[2]
-        case 7 | 8:
-            todays_mood = mood_tag[1]
-        case 9 | 10:
-            todays_mood = mood_tag[0]
-    
-    if todays_mood == "Moderate":
-        commentary = "Moderate score given - No commentary required"
-        print("Moderate score given - No commentary required")
+        # safely get a valid rating
+        while True:
+            try:
+                mood_rating = int(input("Rate 1–10: "))
+                if 1 <= mood_rating <= 10:
+                    break
+                else:
+                    print("Please enter a number between 1 and 10.")
+            except ValueError:
+                print("Please enter a valid number.")
 
-    else:
-        commentary = input("Please add some commentary explaining your answer (250 char max): ")
-    
+        match mood_rating:
+            case 1 | 2:
+                todays_mood = mood_tag[4]
+            case 3 | 4:
+                todays_mood = mood_tag[3]
+            case 5 | 6:
+                todays_mood = mood_tag[2]
+            case 7 | 8:
+                todays_mood = mood_tag[1]
+            case 9 | 10:
+                todays_mood = mood_tag[0]
 
-    # Define data that will be added to the table
-    data = [the_date, todays_mood, commentary]
+        if todays_mood == "Moderate":
+            commentary = "Moderate score given - No commentary required"
+            print("Moderate score given - No commentary required")
 
-    # Insert the latest entries into the DB
-    cur.execute("INSERT INTO MoodRatings (date, moodlabel, commentary) VALUES (?, ?, ?)", data)
-    con.commit()
-    
-    # Closing notes on the script
-    print("Thanks! I've saved your records. What would you like to do next?") 
-    what_next = int(input("Press '1' to view all your entries so far, or Press '0' to exit the program"))
+        else:
+            commentary = input("Please add some commentary explaining your answer (250 char max): ")
 
-    cur.execute("SELECT * FROM MoodRatings;")
-    allrows = cur.fetchall()
+        # Define data that will be added to the table
+        data = [the_date, todays_mood, commentary]
 
-    if what_next == 1:
-        #display database records
-        print("Here are your previous entries: ")
-        for row in cur.execute("SELECT date, moodlabel, commentary FROM MoodRatings"):
-            print(row)
-        
-        print("Would you like a csv or JSON file containing all your previous submissions?")
-        export_file = int(input("If you would like a CSV export - press 1; If you would like to exit without exporting your data - press 0" \
-        "Action: "))
+        # Insert the latest entries into the DB
+        cur.execute("INSERT INTO MoodRatings (date, mood_label, commentary) VALUES (?, ?, ?)", data)
+        con.commit()
 
-        if export_file == 1:
-            print("Exporting data into CSV............")
-            with open("table.csv", "w", newline="", encoding="utf-8") as f:
-                writer = csv.writer(f)
-                writer.writerow([header[0] for header in cur.description])
-                writer.writerows(allrows)
-            print("File successfully exported!!")
+        # Closing notes on the script
+        print("Thanks! I've saved your records. What would you like to do next?")
 
-        elif export_file == 0:
+        while True:
+            try:
+                what_next = int(input("Press '1' to view all your entries so far, or Press '0' to exit the program"))
+                if what_next in (0, 1):
+                    break
+            except ValueError:
+                pass
+            print("Invalid choice")
+
+        if what_next == 1:
+            #display database records
+            print("Here are your previous entries: ")
+            cur.execute("SELECT * FROM MoodRatings;")
+            allrows = cur.fetchall()
+            for row in allrows:
+                print(row)
+
+            print("Would you like a csv file containing all your previous submissions?")
+
+            while True:
+                try:
+                    export_file = int(input(
+                        "If you would like a CSV export - press 1; If you would like to exit without exporting your data - press 0" \
+                        "Action: "))
+                    if export_file in (0, 1):
+                        break
+                except ValueError:
+                    print("Invalid choice. Please select '0' or '1'")
+
+            if export_file == 1:
+                print("Exporting data into CSV............")
+                with open("table.csv", "w", newline="", encoding="utf-8") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(["date", "mood_label", "commentary"])
+                    writer.writerows(allrows)
+                print("File successfully exported!!")
+
+            elif export_file == 0:
+                print("Thanks, see you again tomorrow!")
+
+        elif what_next == 0:
+            #exit function
             print("Thanks, see you again tomorrow!")
 
-    elif what_next == 0:
-        #exit function
-        print("Thanks, see you again tomorrow!")
+        else:
+            print("Invalid entry, please press '1' to view your previous records, or press '0' to exit the program: ")
 
-    else: "Invalid entry, please press '1' to view your previous records, or press '0' to exit the program: "
 
 daily_mood_check()
-
